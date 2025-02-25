91763374950abbc85d9faf0548b82563c0a5bde9
Shray Arora <shrayarora@Shrays-MacBook-Air.local>
2025-02-24 23:24:59 -0800

    Implement non-preemptive SJF scheduling

--- src/simulation.py
+++ src/simulation.py
@@ -1,6 +1,8 @@
 import simpy
 import random
 from scheduler.fcfs import FCFSScheduler
+from scheduler.sjf import SJFScheduler
+
 
 # Simulation parameters
 RANDOM_SEED = 42  
@@ -12,7 +14,7 @@ ARRIVAL_RATE = 5
 SIM_TIME = 100    
 
 # Generates new processes at random intervals
-def process_generator(env, cpu, scheduler):
+def process_generator(env, cpu, scheduler, scheduler_name):
     process_id = 0
     while True:
         yield env.timeout(random.expovariate(1.0 / ARRIVAL_RATE))  
@@ -21,20 +23,29 @@ def process_generator(env, cpu, scheduler):
         name = f'Process-{process_id}'
         env.process(scheduler.process_task(name, burst_time))
 
+# Function to run a specific scheduler
+def run_simulation(scheduler_name):
+    print(f"\nStarting {scheduler_name} simulation...")
+    random.seed(RANDOM_SEED)
+    env = simpy.Environment()
+    cpu = simpy.Resource(env, capacity=1)
+
+    # Choose scheduler
+    if scheduler_name == "FCFS":
+        scheduler = FCFSScheduler(env, cpu)
+    elif scheduler_name == "SJF":
+        scheduler = SJFScheduler(env, cpu)
+    else:
+        raise ValueError("Invalid scheduler name!")
+    
+    env.process(process_generator(env, cpu, scheduler, scheduler_name))
+    env.run(until=SIM_TIME)
+    print(f"{scheduler_name} simulation complete.\n")
+
 # Main simulation function
 def main():
-    random.seed()  
-    env = simpy.Environment()  
-    # Single-core CPU
-    cpu = simpy.Resource(env, capacity=1) 
-
-    fcfs_scheduler = FCFSScheduler(env, cpu) 
-
-    env.process(process_generator(env, cpu,fcfs_scheduler))
-
-    print("Starting FCFS simulation...")
-    env.run(until=SIM_TIME)  # Run for 100 time units
-    print("Simulation complete.")
+    run_simulation("FCFS")
+    run_simulation("SJF")
 
 if __name__ == '__main__':
     main()
